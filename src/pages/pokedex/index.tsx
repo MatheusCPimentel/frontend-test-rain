0ad937a462e9e25024c809cc4e95b96a37a36558
Matheus Pimentel <matheus08.pimentel@gmail.com>
2024-11-22 12:29:02 -0300

    chore: removed filter by type (not possible due to API limitations

--- src/pages/pokedex/index.tsx
+++ src/pages/pokedex/index.tsx
@@ -1,22 +1,11 @@
-import { ChevronDown, Target } from "lucide-react";
 import styles from "./styles.module.css";
 import { useEffect, useState } from "react";
 import { Toggle } from "../../components/Toggle";
 import { fetchWrapper } from "../../services/api";
-import { Pagination } from "../../types/pagination";
-import { PokemonType } from "../../types/pokemonType";
-import { capitalizeWord } from "../../utils/capitalizeWord";
 
 export function Pokedex() {
-  const [isTypeModalOpen, setIsTypeModalOpen] = useState(false);
   const [hasToShowOnlyFavorites, setHasToShowOnlyFavorites] = useState(false);
   const [pokemonInputValue, setPokemonInputValue] = useState("");
-
-  const [selectedPokemonType, setSelectedPokemonType] = useState<string | null>(
-    null
-  );
-
-  const [pokemonTypeOptions, setPokemonTypeOptions] = useState<string[]>([]);
 
   const [pokemonsToShow, setPokemonsToShow] = useState([]);
 
@@ -30,17 +19,8 @@ export function Pokedex() {
     const response = await fetchWrapper("pokemon");
   };
 
-  const getAllPokemonTypes = async () => {
-    const { data } = await fetchWrapper<Pagination<PokemonType>>("type");
-
-    const allAvailableTypes = data.results.map((type) => type.name);
-
-    setPokemonTypeOptions(allAvailableTypes);
-  };
-
   useEffect(() => {
     getPokemons();
-    getAllPokemonTypes();
   }, []);
 
   return (
@@ -57,53 +37,11 @@ export function Pokedex() {
           placeholder="Find your Pokémon..."
         />
 
-        <div className={styles.filterOptions}>
-          <div className={styles.typeContainer}>
-            <div
-              className={styles.typeInput}
-              onClick={() => setIsTypeModalOpen(!isTypeModalOpen)}
-            >
-              <Target size={16} />
-              <span>Type</span>
-              <ChevronDown size={16} />
-            </div>
-
-            {isTypeModalOpen && (
-              <div className={styles.typeModal}>
-                {pokemonTypeOptions.map((type) => (
-                  <label
-                    key={type}
-                    className={styles.typeOption}
-                    onClick={(e) => e.stopPropagation()}
-                  >
-                    <input
-                      type="radio"
-                      name="pokemonType"
-                      value={type}
-                      checked={selectedPokemonType === type}
-                      onChange={() => setSelectedPokemonType(type)}
-                    />
-
-                    <span>{capitalizeWord(type)}</span>
-                  </label>
-                ))}
-
-                <button
-                  className={styles.applyButton}
-                  onClick={() => setIsTypeModalOpen(false)}
-                >
-                  Apply
-                </button>
-              </div>
-            )}
-          </div>
-
-          <Toggle
-            isChecked={hasToShowOnlyFavorites}
-            setIsChecked={setHasToShowOnlyFavorites}
-            label="Show only my favorites"
-          />
-        </div>
+        <Toggle
+          isChecked={hasToShowOnlyFavorites}
+          setIsChecked={setHasToShowOnlyFavorites}
+          label="Show only my favorites"
+        />
       </header>
 
       <main>
--- src/types/pokemonType.ts
+++
@@ -1,4 +0,0 @@
-export interface PokemonType {
-  name: string;
-  url: string;
-}
