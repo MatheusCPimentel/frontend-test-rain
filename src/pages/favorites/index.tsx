a6448f868b8256486e79b457539f491d39940c3b
Matheus Pimentel <matheus08.pimentel@gmail.com>
2024-11-21 21:06:06 -0300

    chore: added router and the 3 pages

---
+++ src/pages/favorites/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.css";
+
+export function Favorites() {
+  return (
+    <div>
+      <h1>Favorites</h1>
+    </div>
+  );
+}
---
+++ src/pages/main/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./styles.module.css";
+
+export function Main() {
+  return (
+    <div>
+      <h1>Main</h1>
+    </div>
+  );
+}
